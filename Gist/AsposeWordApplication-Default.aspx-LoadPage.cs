84e7cf40c8d2cc7ebf42f6900486bff9d7a98c20
Muhammad Atir <atirtahir@hotmail.com>
2015-11-22 22:47:41 +0500

    balbla

--- Gist/AsposeWordApplication-Default.aspx-LoadPage.cs
+++ Gist/AsposeWordApplication-Default.aspx-LoadPage.cs
@@ -1,10 +1,14 @@
 // For complete examples and data files, please go to https://github.com/atirtahir/PDF-invoice-generator-using-aspose.word-api
-if (!Page.IsPostBack)
-{
-    List<int> lst = new List<int>();
-    lst.Add(1);
-    lst.Add(2);
-    lst.Add(3);
-    ItemDetails.DataSource = lst;
-    ItemDetails.DataBind();
-}
+
+            //new code
+            //another
+
+            if (!Page.IsPostBack)
+            {
+                List<int> lst = new List<int>();
+                lst.Add(1);
+                lst.Add(2);
+                lst.Add(3);
+                ItemDetails.DataSource = lst;
+                ItemDetails.DataBind();
+            }
